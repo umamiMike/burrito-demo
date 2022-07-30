3ec2a527713111e4cb8c6a6042372984c51adef7
Michael Wilding <heymichaelwilding@gmail.com>
2022-07-30 14:20:35 -0700

    updates to css and Cart

--- client/src/Cart.js
+++ client/src/Cart.js
@@ -8,20 +8,18 @@ export const Cart = () => {
   let price = cart.price ? "$" + cart.price.toFixed(2) : "";
 
   const toppings = () => {
-    console.log(cart);
+    if (cart.toppings)
+      return `You are ordering a ${ctx.stateValue.cart.name}
+        with ${cart.toppings.join(", ")}
+         for   ${price}`;
     return " ";
   };
+
   return (
-    <div className="fixed  bottom-0 flex-row  p-4 bg-gray-200 gap-3 transition-all duration-100 ease-in-out w-screen h-1/4 ">
+    <div className="fixed border-t-[1px] border-gray-400  bottom-0 flex-row  p-4 bg-gray-200 gap-3 transition-all duration-100 ease-in-out w-screen h-1/4 ">
       <div className="border-b-[1px] border-gray-400 border-solid ">Cart</div>
       <div className="font-bold"></div>
-      <p>
-        {" "}
-        {`You are ordering a ${ctx.stateValue.cart.name}`}
-        <br />
-        {`with ${cart.toppings.join(", ")}`}
-        {` for   ${price}`}
-      </p>
+      <p>{toppings()}</p>
       <button className="text-white bg-yellow-500 rounded-full">
         place your order
       </button>
