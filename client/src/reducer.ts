40dd4f2f2ebdd45bb8856c91ecf1123ad02f2884
Michael W.C. Wilding <michael.w.chastain@gmail.com>
2022-08-28 16:37:41 -0700

    UPDATES type to whitelist specific actions

--- client/src/reducer.ts
+++ client/src/reducer.ts
@@ -1,7 +1,7 @@
 import type { Shop } from './baseState';
 
 export type Action = {
-  type: string;
+  type: 'START_ORDER' | 'ADD_TOPPING' | 'REMOVE_TOPPING' | 'CANCEL_ORDER' | 'ADD_TO_CART';
   payload: any;
 };
 
@@ -21,9 +21,6 @@ export const shopReducer = (state: Shop, action: Action) => {
       return n;
       break;
     case 'ADD_TOPPING':
-      // if checked add the string
-      // if checked and exists in array remove string from array
-
       return {
         ...state,
         selected: {
@@ -35,7 +32,6 @@ export const shopReducer = (state: Shop, action: Action) => {
       break;
     case 'REMOVE_TOPPING':
 
-      console.log('remove topping ');
       return {
         ...state,
         selected: {
