c9a49a79457b9dc6e1392976ee6d6fcbb0b4e23e
Michael W.C. Wilding <michael.w.chastain@gmail.com>
2022-11-11 11:04:20 -0800

    updates. mostly to client

--- client/src/MenuCard.tsx
+++ client/src/MenuCard.tsx
@@ -1,17 +1,25 @@
+import React, { useContext } from 'react';
 import type { MenuItem } from './baseState';
 
 export function MenuCard(item: MenuItem) {
   return (
-
-    <div>
-      <a href="/" className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl dark:border-gray-300 ">
-        <img className="object-cover w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/images/burrito.png" alt="" />
+      <a
+        href="/"
+        className="flex flex-col items-center bg-white border rounded-lg shadow-md md:flex-row md:max-w-xl dark:border-gray-300"
+      >
+        <img
+          className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src="/images/burrito.png"
+          alt=""
+        />
         <div className="flex flex-col justify-between p-4 leading-normal">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-500 ">Heading</h5>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">menus item description</p>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-500">
+            Heading
+          </h5>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            menus item description
+          </p>
         </div>
       </a>
-    </div>
-
   );
 }
---
+++ messagingService/go.mod
@@ -0,0 +1,22 @@
+module example.com/m/v2
+
+go 1.17
+
+require (
+	github.com/alash3al/go-pubsub v0.0.0-20200412052039-02569461a126 // indirect
+	github.com/alash3al/wsify v0.0.0-20181019133613-cf333d85237b // indirect
+	github.com/bclicn/color v0.0.0-20180711051946-108f2023dc84 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/rs/xid v1.4.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
+	golang.org/x/net v0.0.0-20220920203100-d0c6ba3f52d9 // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
